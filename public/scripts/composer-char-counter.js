cbdfdcf7e848b0c0f875fd23f1bd553ff525a174
AVacAdok <avakuang8@gmail.com>
2022-06-21 23:45:56 +0000

    implement word counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+
+  countChar = function (words) {
+    let charLength = words.value.length;
+    $(".counter").text(140 - charLength);
+    if (charLength > 140) {
+      $("#counter").css("color", "#e5c959")
+    } else {
+      $("#counter").css("color", "#4b4b37")
+    }
+  }
+});
